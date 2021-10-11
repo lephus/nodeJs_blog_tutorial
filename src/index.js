beb945f88293d52e6571a608ec70a0f49941066c
LeHuuPhu <phulealali@gmail.com>
2021-10-11 09:30:57 +0700

    handlebar and scss

--- src/index.js
+++ src/index.js
@@ -7,11 +7,12 @@ const handlebars = require('express-handlebars');
 const app = express();
 const port = 3000;
 
+app.use(express.static(path.join(__dirname,'public')))
 
 app.use(morgan('combined'));
 
-app.engine('handlebars', handlebars());
-app.set('view engine', 'handlebars');
+app.engine('hbs', handlebars({extname: '.hbs'}));
+app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
 
@@ -19,6 +20,11 @@ app.set('views', path.join(__dirname, 'resources/views'));
 app.get('/', (req, res) => {
   res.render('home');
 });
+app.get('/news', (req, res) => {
+  res.render('news');
+});
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
