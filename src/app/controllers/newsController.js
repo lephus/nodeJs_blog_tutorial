3dfdd8c44c7ab15a4cfd8d2c6ae83d683b245f6e
LeHuuPhu <phulealali@gmail.com>
2021-11-08 08:49:17 +0700

    client render side promises

--- src/app/controllers/newsController.js
+++ src/app/controllers/newsController.js
@@ -4,14 +4,20 @@ const Course = require('../models/Course');
 class NewsController {
   //GET /news
   index(req, res, next) {
-    Course.find({}, function (error, Courses) {
-      if(!error){
-        res.json(Courses);
-      }else{
-        next(error);
-        //res.status(400).json({error: "can not get data course"})
-      }
-    });
+    Course.find({})
+      .then(course => res.json(course))
+      .catch(error => next(error));
+      
+    // Course.find({}, function (error, Courses) {
+    //   if(!error){
+    //     res.json(Courses);
+    //   }else{
+    //     next(error);
+    //     //res.status(400).json({error: "can not get data course"})
+    //   }
+    // });
+
+
 
     //res.render('news');
   }
